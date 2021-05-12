91fb8a039c1d822e631409b86482b2245d355850
nlumiere <nicholaslumiere@gmail.com>
2021-05-12 13:46:01 -0700

    adds locally-hostable node backend and firebase-hosted public site

---
+++ public/scripts.js
@@ -0,0 +1,113 @@
+
+const canvasWidth = 512;
+const canvasHeight = 512;
+var COLOR = "#ff0000";
+
+
+window.onload = function() {
+    loadCanvas()
+}
+
+function wsinit() {
+    const board = document.querySelector('#canvas');
+
+    let ws;
+
+    async function init() {
+        if (ws) {
+            ws.onerror = ws.onopen = ws.onclose = null;
+            ws.close();
+        }
+
+        ws = new WebSocket('ws://localhost:6969');
+        ws.onopen = () => {
+            console.log('Connection opened!');
+        }
+        ws.onmessage = ({ data }) => {
+            parsed_data = JSON.parse(data);
+            x = parsed_data.x;
+            y = parsed_data.y;
+            r = parsed_data.r;
+            g = parsed_data.g;
+            b = parsed_data.b;
+            var c = document.getElementById("canvas");
+            var ctx = c.getContext("2d");
+            var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+
+            var index = (x + y * canvasWidth) * 4;
+
+            canvasData.data[index + 0] = r;
+            canvasData.data[index + 1] = g;
+            canvasData.data[index + 2] = b;
+            canvasData.data[index + 3] = 255;
+            ctx.putImageData(canvasData, 0, 0);
+        };
+        ws.onclose = function() {
+            ws = null;
+        }
+
+        return 1;
+    }
+
+    board.onclick = function() {
+        color = document.getElementById("picker");
+        var x = (event.pageX - $('#canvas').offset().left);
+        var y = event.pageY - $('#canvas').offset().top;
+        x = Math.floor((x - 2));
+        y = Math.floor((y - 2));
+        var r = parseInt("0x" + COLOR.substring(1,3));
+        var g = parseInt("0x" + COLOR.substring(3,5));
+        var b = parseInt("0x" + COLOR.substring(5));
+        var time = new Date().getTime();
+        if(x >= 0 && x < 512 && y >= 0 && y < 512){
+            ws.send(JSON.stringify(
+                {
+                    'x':x,
+                    'y':y,
+                    'r':r,
+                    'g':g,
+                    'b':b,
+                    'datetime': time
+                }
+            ));
+        }
+    }
+    
+    init();
+    loadBoard();
+}
+
+function loadBoard(){
+    console.log("loading board");
+    board = document.getElementById("canvas");
+    board.width = canvasWidth;
+    board.height = canvasHeight;
+    var colorPicker = new iro.ColorPicker("#picker", {
+        width: 240,
+        color: "#ff0000"
+    });
+
+    colorPicker.on('color:change', function(color) {
+        COLOR = color.hexString;
+    });
+}
+
+function loadCanvas(){
+    console.log("loading canvas");
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+    const db = firebase.firestore();
+    db.collection("pixels").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            pixeldata = doc.data();
+            var index = (pixeldata.x + pixeldata.y * canvasWidth) * 4;
+
+            canvasData.data[index + 0] = pixeldata.r;
+            canvasData.data[index + 1] = pixeldata.g;
+            canvasData.data[index + 2] = pixeldata.b;
+            canvasData.data[index + 3] = 255;
+        });
+        ctx.putImageData(canvasData, 0, 0);
+    });
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+// const firebase = require('firebase');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
+
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+
+        parsed_data = JSON.parse(data);
+
+        db.collection("pixels").doc("x" + parsed_data.x + "y" + parsed_data.y).set(parsed_data, {merge: true});
+        db.collection("updates").add(parsed_data);
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    })
+})
+   
+
+server.listen(port, function() {
+    console.log(`Server is listening on ${port}!`)
+})
